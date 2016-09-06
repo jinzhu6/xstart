219ccd0e414a5d5d026c733ac762c5ad14bd0d5e
halem <halem@huettinger.de>
2016-09-06 12:43:48 +0200

    removed some unneccessary files

--- build-cmake.bat
+++
@@ -1,9 +0,0 @@
-REM echo.
-REM echo *****************************************************************************
-REM echo CMAKE WILL NOW DO ITS JOB ...
-REM echo *****************************************************************************
-mkdir build
-cd build
-cmake .. -DMSVC_RUNTIME=static
-cd ..
-pause
